1a11d6e5dea40446779d32a026186cc037f52ac5
Roberto <roberto.bem@dotgroup.com.br>
2018-12-03 14:05:11 -0200

    SPA AXIOS

---
+++ src/components/TableUsers.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    teste
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios';
 
 Vue.config.productionTip = false
 
+const http = Axios.create({
+  baseURL: 'localhost:8081'
+})
+
 new Vue({
   render: h => h(App),
+  http,
 }).$mount('#app')
